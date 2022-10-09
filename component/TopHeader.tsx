e9e3284a6686f91e8c368fc4c07ee8b08080fbf3
Eftekar Raghib <70286969+juniorEdris@users.noreply.github.com>
2022-10-09 20:00:09 +0600

    border design added

--- component/TopHeader.tsx
+++ component/TopHeader.tsx
@@ -4,7 +4,7 @@ import MenuDropDown from "../utils/dropdowns/MenuDropDown";
 
 const TopHeader: NextPage = () => {
   return (
-    <div className="flex py-2 items-center">
+    <div className="flex py-2 items-center border-b-2 border-b-picton mb-5">
       <div className="w-full lg:w-1/2 text-sm md:font-medium">Friday, September 10</div>
       <div className="w-full lg:w-1/2 flex gap-5 items-center justify-end">
         <MenuDropDown
